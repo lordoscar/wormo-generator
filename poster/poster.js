e7eb50e5a0af5443ba48955a421199c1c7dae3f2
Oscar Andersson <oscar.andersson3@ikea.com>
2019-05-07 08:52:43 +0200

    url to blob

--- poster/poster.js
+++ poster/poster.js
@@ -209,11 +209,20 @@ function saveImage() {
     if (screenShot) {
         var link = document.createElement("a");
         link.download = name;
-        link.href = canvas.toDataURL("image/png");
+        link.href = dataURLtoBlob(canvas.toDataURL("image/png"));
         console.log(link.href);
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
         delete link;
     }
+}
+
+function dataURLtoBlob(dataurl) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], {type:mime});
 }
